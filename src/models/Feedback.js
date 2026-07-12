a6d486f49be02931468e985a9a148ee27d227686
Adevos <adevostech@gmail.com>
2026-07-12 06:44:17 +0300

    Create Feedback.js

---
+++ src/models/Feedback.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const feedbackSchema = new mongoose.Schema(
+  {
+    text: { type: String, required: true },
+    author: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Feedback', feedbackSchema);
